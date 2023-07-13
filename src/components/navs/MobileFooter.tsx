852959b4a6e2e786ce843e8afdc5f05658629557
min33sky <min33sky@naver.com>
2023-07-13 22:00:15 +0900

    feat: add mobile nav item component

--- src/components/navs/MobileFooter.tsx
+++ src/components/navs/MobileFooter.tsx
@@ -1,9 +1,29 @@
+'use client';
+
+import useRoutes from '@/hooks/useRoutes';
 import React from 'react';
+import MobileNavItem from './MobileNavItem';
 
 export default function MobileFooter() {
+  const routes = useRoutes();
+
+  // const { isOpen } = useConversation();
+
+  // if (isOpen) {
+  //   return null;
+  // }
+
   return (
     <footer className="fixed bottom-0 z-40 flex w-full items-center justify-between border-t-[1px] bg-white lg:hidden">
-      MobileFooter
+      {routes.map((route) => (
+        <MobileNavItem
+          key={route.href}
+          href={route.href}
+          active={route.active}
+          icon={route.icon}
+          onClick={route.onClick}
+        />
+      ))}
     </footer>
   );
 }
